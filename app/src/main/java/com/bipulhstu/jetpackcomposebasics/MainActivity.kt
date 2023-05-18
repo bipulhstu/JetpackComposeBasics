a71d68d65204f3f9ffe68241214090a3670ab623
Bipul Islam <bipulhstu@gmail.com>
2023-05-18 17:56:02 +0600

    starter tutorial finished

--- app/src/main/java/com/bipulhstu/jetpackcomposebasics/MainActivity.kt
+++ app/src/main/java/com/bipulhstu/jetpackcomposebasics/MainActivity.kt
@@ -1,32 +1,67 @@
 package com.bipulhstu.jetpackcomposebasics
 
+import SampleData
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.bipulhstu.jetpackcomposebasics.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val painter = painterResource(id = R.drawable.snow)
+            /*JetpackComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Mr. Bipul Islam")
+                }
+            }*/
+
+            /*val painter = painterResource(id = R.drawable.snow)
             val description = "Kermit in the snow"
             val title = "Kermit is playing in the snow"
             Box(
@@ -35,8 +70,155 @@ class MainActivity : ComponentActivity() {
                     .padding(16.dp)
             ) {
                 ImageCard(painter = painter, contentDescription = description, title = title)
+            }*/
+
+            JetpackComposeTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    //MessageCard(Message("Android", "Jetpack Compose"))
+                    Conversation(messages = SampleData.conversationSample)
+                }
+            }
+
+
+        }
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message = message)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    JetpackComposeTheme {
+        Conversation(messages = SampleData.conversationSample)
+    }
+}
+
+
+data class Message(val author: String, val body: String)
+
+
+@Composable
+fun MessageCard(message: Message) {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        // We keep track if the message is expanded or not in this
+        // variable
+        var isExpanded by remember { mutableStateOf(false) }
+
+        //surfaceColor will be updated gradually from one color to another
+
+        val surfaceColor by animateColorAsState(targetValue = if(isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface)
+
+        Row(modifier = Modifier
+            .padding(8.dp)
+            .clickable { isExpanded = !isExpanded }) {
+            Image(
+                painter = painterResource(id = R.drawable.profile_picture),
+                contentDescription = "Profile Picture",
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+                    .border(1.5.dp, MaterialTheme.colors.primary, CircleShape)
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Column {
+                Text(
+                    text = message.author,
+                    color = MaterialTheme.colors.secondary,
+                    style = MaterialTheme.typography.subtitle1
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Surface(
+                    shape = MaterialTheme.shapes.medium,
+                    elevation = 1.dp,
+                    //surface color will be changing gradually
+                    color = surfaceColor,
+                    //animateContentSize will change the surface size gradually
+                modifier = Modifier.animateContentSize().padding(1.dp)
+                ) {
+                    Text(
+                        text = message.body,
+                        modifier = Modifier.padding(all = 4.dp),
+                        //If the message is expanded, we display all its content
+                        //otherwise we only display the first line
+                        maxLines = if(isExpanded) Int.MAX_VALUE else 1,
+                        style = MaterialTheme.typography.body1
+                    )
+                }
             }
         }
+    }
+}
+
+
+/*@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)*/
+@Composable
+fun PreviewMessageCard() {
+    JetpackComposeTheme {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            Row(modifier = Modifier.padding(8.dp)) {
+                Image(
+                    painter = painterResource(id = R.drawable.profile_picture),
+                    contentDescription = "Profile Picture",
+                    modifier = Modifier
+                        .size(40.dp)
+                        .clip(CircleShape)
+                        .border(1.5.dp, MaterialTheme.colors.primary, CircleShape)
+                )
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+                Column {
+                    Text(
+                        text = "Android",
+                        color = MaterialTheme.colors.secondary,
+                        style = MaterialTheme.typography.subtitle1
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(
+                        text = "Jetpack Compose",
+                        style = MaterialTheme.typography.body1
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun Greeting(name: String) {
+    Text(
+        text = "Hello $name!",
+        color = Color.Blue,
+        fontSize = 38.sp,
+        modifier = Modifier
+            .background(Color.Gray)
+            .padding(16.dp)
+            .background(Color.Green)
+
+    )
+}
+
+
+@Composable
+fun DefaultPreview() {
+    JetpackComposeTheme {
+        Greeting("Mr. Bipul Islam")
     }
 }
 
@@ -49,7 +231,11 @@ fun ImageCard(
 ) {
     Card(
         shape = RoundedCornerShape(15.dp),
-        modifier = modifier.fillMaxWidth().clickable {  },
+        modifier = modifier
+            .fillMaxWidth()
+            .clickable {
+
+            },
         elevation = 5.dp,
     ) {
         Box(modifier = Modifier.height(200.dp)) {
